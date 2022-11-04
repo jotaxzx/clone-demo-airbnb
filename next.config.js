dbc3c1160375a66e84028dd372181aba98f4822b
Jose Trujillo <joseantoniotsandoval@gmail.com>
2022-11-04 02:10:22 -0300

    first config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
   },
   env: {
     mapbox_key: 'pk.eyJ1IjoieHplcHAiLCJhIjoiY2xhMGpuODh1MDB1OTNvbDgxZHN3ODQ1MiJ9.0m9KN3Ku0_gefm3ugpyPvQ'
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   }
 }
 
